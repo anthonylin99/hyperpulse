1c973f00412779ace9e92a9978d43942e6933a2d
anthonylin99 <anthony.lin@artemsanalytics.xyz>
2026-04-02 17:53:43 -0400

    Defer client providers to avoid build-time Privy init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Providers from "./providers";
+import dynamic from "next/dynamic";
 import "./globals.css";
+
+const ClientProviders = dynamic(() => import("./providers"), { ssr: false });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
   );
